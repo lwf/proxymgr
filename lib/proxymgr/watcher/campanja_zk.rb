e6709860e7ca9af2832a1d0a2b08dc0158f45cf5
Torbjörn Norinder <torbjorn@campanja.com>
2014-06-30 15:14:00 +0200

    Move wait_for_path to ZKClient, rename to when_path

--- lib/proxymgr/watcher/campanja_zk.rb
+++ lib/proxymgr/watcher/campanja_zk.rb
@@ -9,42 +9,11 @@ module ProxyMgr
         when ::Zookeeper::ZOK
           update_servers(req[:children])
         when ::Zookeeper::ZNONODE
-          wait_for_path(@config['path'])
+          @zookeeper.when_path(@config['path']) { watch }
         else
           logger.warn "get_children returned #{req[:rc].to_s}"
         end
       end
-
-      def wait_for_path(path, rest = [])
-        cb = ::Zookeeper::Callbacks::WatcherCallback.new do |event|
-          next_path = join(path, rest.first)
-          if @zookeeper.get(:path => next_path)[:rc] == ::Zookeeper::ZOK
-            if next_path == @config['path']
-              logger.debug "#{@config['path']} now exists, watching it"
-              watch
-            else
-              logger.debug "#{next_path} exists, moving on to next"
-              rest.shift
-              wait_for_path(next_path, rest)
-            end
-          else
-            wait_for_path(path, rest)
-          end
-        end
-
-        req = @zookeeper.get_children(:path => path, :watcher => cb)
-        case req[:rc]
-        when ::Zookeeper::ZOK
-          logger.debug "Now watching #{path}"
-        when ::Zookeeper::ZNONODE
-          logger.debug "Re-resolving paths"
-          path, rest = find_wait_path
-          wait_for_path(path, rest)
-        else
-          logger.warn "wait_for_path #{path} failed: #{req[:rc].to_s}"
-        end
-      end
-
 
       def update_servers(children)
         servers = children.map { |child| "#{child}:#{@config['port']}" }.sort
@@ -52,35 +21,6 @@ module ProxyMgr
           @servers = servers
           @manager.update_backends
         end
-      end
-
-      def find_wait_path
-        parts = split(@config['path'])
-        rest = [parts.pop]
-        path = '/'
-        until parts.empty?
-          path = join(*parts)
-          if @zookeeper.get(:path => path)[:rc] == ::Zookeeper::ZOK
-            break
-          end
-          rest.unshift parts.pop
-        end
-        [path, rest]
-      end
-
-      def join(*path)
-        # TODO: deal with broken path input
-        p = ::File.join(*path)
-        if p == ""
-          "/"
-        else
-          p
-        end
-      end
-      
-      def split(path)
-        head, *tail = path.split("/")
-        [head] + tail.reject(&:empty?)
       end
     end
   end
--- lib/proxymgr/zkclient.rb
+++ lib/proxymgr/zkclient.rb
@@ -2,9 +2,6 @@ module ProxyMgr
   class ZKClient 
     require 'zookeeper'
     require 'state_machine'
-    require 'forwardable'
-
-    extend Forwardable
 
     include Logging
 
@@ -46,8 +43,6 @@ module ProxyMgr
 
     attr_accessor :on_expired, :on_connected, :on_disconnected
 
-    def_delegators :@zookeeper, :get_children, :get, :create, :delete
-
     def initialize(servers = 'localhost:2181', opts = {})
       super()
 
@@ -80,6 +75,74 @@ module ProxyMgr
       if blk = send(event_type)
         blk.call
       end
+    end
+
+    def when_path(path, &blk)
+      wait_for_path(path, path, [], &blk)
+    end
+
+    def method_missing(sym, *args, &blk)
+      @zookeeper.send(sym, *args, &blk)
+    end
+
+    private
+
+    def wait_for_path(complete_path, wait_path, rest = [], &blk)
+      cb = ::Zookeeper::Callbacks::WatcherCallback.new do |event|
+        next_path = join(wait_path, rest.first)
+        if @zookeeper.get(:path => next_path)[:rc] == ::Zookeeper::ZOK
+          if next_path == complete_path
+            logger.debug "#{current_path} now exists, watching it"
+            blk.call
+          else
+            logger.debug "#{next_path} exists, moving on to next"
+            rest.shift
+            wait_for_path(complete_path, next_path, rest, &blk)
+          end
+        else
+          wait_for_path(complete_path, wait_path, rest, &blk)
+        end
+      end
+
+      req = @zookeeper.get_children(:path => wait_path, :watcher => cb)
+      case req[:rc]
+      when ::Zookeeper::ZOK
+        logger.debug "Now watching #{wait_path}"
+      when ::Zookeeper::ZNONODE
+        logger.debug "Re-resolving paths"
+        wait_path, rest = find_wait_path(complete_path)
+        wait_for_path(complete_path, wait_path, rest, &blk)
+      else
+        logger.warn "wait_for_path #{wait_path} failed: #{req[:rc].to_s}"
+      end
+    end
+
+    def find_wait_path(complete_path)
+      parts = split(complete_path)
+      rest = [parts.pop]
+      path = '/'
+      until parts.empty?
+        path = join(*parts)
+        if @zookeeper.get(:path => path)[:rc] == ::Zookeeper::ZOK
+          break
+        end
+        rest.unshift parts.pop
+      end
+      [path, rest]
+    end
+
+    def join(*path)
+      p = ::File.join(*path)
+      if p == ""
+        "/"
+      else
+        p
+      end
+    end
+
+    def split(path)
+      head, *tail = path.split("/")
+      [head] + tail.reject(&:empty?)
     end
   end
 end
