95bd5549438feeeab2b8562666f30aa3e648dca6
Torbjörn Norinder <torbjorn@campanja.com>
2014-07-01 20:46:06 +0200

    Fix recursive call in watch which was causing a race condition to crash the Zookeeper thread

--- lib/proxymgr/watcher/zookeeper.rb
+++ lib/proxymgr/watcher/zookeeper.rb
@@ -2,7 +2,7 @@ module ProxyMgr
   module Watcher
     class Zookeeper < Base
       def watch
-        @zookeeper = ZKClient.new
+        @zookeeper = ZKClient.new(@config['server'])
         @zookeeper.on_connected { watch_zookeeper }
         @zookeeper.connect
       end
@@ -15,7 +15,7 @@ module ProxyMgr
 
       def watch_zookeeper
         # TODO: synchronized. watch could be called from the zookeeper thread while we're running
-        cb = ::Zookeeper::Callbacks::WatcherCallback.new { |event| watch }
+        cb = ::Zookeeper::Callbacks::WatcherCallback.new { |event| watch_zookeeper }
         req = @zookeeper.get_children(:path => @config['path'], :watcher => cb)
         case req[:rc] 
         when ::Zookeeper::ZOK
