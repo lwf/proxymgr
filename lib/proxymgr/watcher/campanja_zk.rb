940b6145b3536c8ddc8a658001633069e23944bb
Torbjörn Norinder <torbjorn@campanja.com>
2014-07-15 12:16:27 +0200

    Fix campanja_zk to conform to new ZK watcher interface

--- lib/proxymgr/watcher/campanja_zk.rb
+++ lib/proxymgr/watcher/campanja_zk.rb
@@ -1,9 +1,17 @@
 module ProxyMgr
   module Watcher
     class Campanjazk < Zookeeper
+      def watch_zookeeper(path, type, req)
+        if type == :update
+          @zk_mapping[path] = ::File.basename(path)
+        else
+          @zk_mapping.delete(path)
+        end
+        update_servers(@zk_mapping.values.sort)
+      end
+
       def update_servers(children)
         servers = children.map { |child| "#{child}:#{@config['port']}" }.sort
-        return if @servers == servers
         @servers = servers
         @manager.update_backends
       end
