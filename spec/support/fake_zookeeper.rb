617552e8ce51f8ab0062357ac5fe33d644a32077
Torbjörn Norinder <torbjorn@campanja.com>
2014-07-20 16:15:12 +0200

    Initial zk-testing stuff

--- spec/support/fake_zookeeper.rb
+++ spec/support/fake_zookeeper.rb
@@ -5,6 +5,9 @@ class FakeZookeeper
     @server    = server
     @heartbeat = heartbeat
     @watcher   = watcher
+
+    @state     = {:children => {}}
+    @watches   = {}
   end
 
   def reopen
@@ -24,7 +27,116 @@ class FakeZookeeper
     call_session(Zookeeper::ZOO_CONNECTED_STATE)
   end
 
+  def get(opts)
+    path    = opts[:path]
+    watcher = opts[:watcher]
+
+    data = get_path(path)
+    if data
+      watch_path(path, watcher) if watcher
+      event(data)
+    else
+      event(data, Zookeeper::ZNONODE)
+    end
+  end
+
+  def set(opts)
+    path    = opts[:path]
+    data    = opts[:data]
+
+    data = set_path(path, data)
+    fire_watches(path) if data
+    event(data)
+  end
+
+  def create(opts)
+    path    = opts[:path]
+    data    = opts[:data]
+    watcher = opts[:watcher]
+
+    parts  = path.split('/')
+    name   = parts.pop
+    parent = File.join(*parts)
+
+    data = create_path(name, parent, data)
+    fire_watches(parent) if data
+    watch_path(path, watcher) if watcher
+    event(data)
+  end
+
+  def get_children(opts)
+    path    = opts[:path]
+    watcher = opts[:watcher]
+
+    data = get_children_path(path)
+    if data
+      watch_path(path, watcher) if watcher
+      {:rc       => Zookeeper::ZOK,
+       :children => data}
+    else
+      {:rc       => Zookeeper::ZNONODE}
+    end
+  end
+
+  def watcher_callback(&blk)
+    blk
+  end
+
   private
+
+  def get_path(path)
+    node = resolve_node(path)
+    node[:data] if node
+  end
+
+  def set_path(path, set_data)
+    node = resolve_node(path)
+    node[:data] = set_data if node
+  end
+
+  def watch_path(path, watcher)
+    node = resolve_node(path)
+    if node
+      node[:watches] ||= []
+      node[:watches] << watcher
+    end
+  end
+
+  def create_path(name, parent, data)
+    node = resolve_node(parent)
+    if node
+      c = node[:children] ||= {}
+      n = c[name] ||= {}
+      if data
+        n[:data] = data
+      else
+        true
+      end
+    end
+  end
+
+  def get_children_path(path)
+    node = resolve_node(path)
+    node[:children].keys if node and node[:children]
+  end
+
+  def fire_watches(path, event = nil)
+    node = resolve_node(path)
+    node.delete(:watches).each { |w| w.call event } if node and node[:watches]
+  end
+
+  def resolve_node(path)
+    parts = path.split('/')
+    return @state if parts.size <= 1
+    parts.shift
+    leaf_name = parts.pop
+    r = parts.inject(@state[:children]) do |state, comp|
+      data = state[comp]
+      break unless data and data[:children]
+      data[:children]
+    end
+    r[leaf_name] if r
+  end
 
   def call_session(state)
     @watcher.call(zoo_session_event(state))
@@ -32,5 +144,9 @@ class FakeZookeeper
 
   def zoo_session_event(state)
     {:state => state}
+  end
+
+  def event(data, rc = Zookeeper::ZOK)
+    {:data => data, :rc => rc}
   end
 end
--- spec/unit/zk/client_spec.rb
+++ spec/unit/zk/client_spec.rb
@@ -5,10 +5,27 @@ describe ProxyMgr::ZK::Client do
     @client  = ProxyMgr::ZK::Client.new('localhost', {}, FakeZookeeper)
     @client.connect
     @fake_zk = @client.instance_variable_get(:@zookeeper)
+    @client.stub(:watcher_callback) { |&blk| @fake_zk.watcher_callback(&blk) }
   end
 
   it 'tries to reconnect when sessions have expired' do
     expect(@fake_zk).to receive(:reopen)
     @fake_zk.expired!
+  end
+
+  describe '#when_path' do
+    #it 'calls block immediately when path exists' do
+    #  @fake_zk.create(:path => '/a', :data => 'test')
+    #  called = false
+    #  @client.when_path('/a') { called = true }
+    #  called.should == true
+    #end
+
+    it 'recursively places watches when path is not available' do
+      @fake_zk.create(:path => '/a')
+      expect(@fake_zk).to receive(:get_children)
+      called = false
+      @client.when_path('/a/b/c') { called = true }
+    end
   end
 end
